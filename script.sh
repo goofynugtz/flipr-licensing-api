fad65c3b1ec40e7e23ec6eac3099baf55a43291f
goofynugtz <rahulranjan25.RR@gmail.com>
2023-05-17 15:29:51 +0530

    Minor changes

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 # python manage.py collectstatic --no-input
-
+python manage.py createsuperuser --no-input --username ubuntu --email rahulranjan25.RR@gmail.com
 # gunicorn fliprLicensingApi.wsgi:application --bind 0.0.0.0:8000
 python manage.py runserver 0.0.0.0:8000
