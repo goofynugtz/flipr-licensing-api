29752877c7a366202141960ed3a54aeaa9d9b086
goofynugtz <rahulranjan25.RR@gmail.com>
2023-05-17 15:01:05 +0530

    Removed gunicorn for testing

--- script.sh
+++ script.sh
@@ -3,4 +3,5 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 # python manage.py collectstatic --no-input
 
-gunicorn fliprLicensingApi.wsgi:application --bind 0.0.0.0:8000
+# gunicorn fliprLicensingApi.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
