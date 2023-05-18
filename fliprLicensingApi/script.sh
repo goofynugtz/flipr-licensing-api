efc5946487a9146c0a951cce28ba94da67c59f68
goofynugtz <rahulranjan25.RR@gmail.com>
2023-05-18 23:30:42 +0530

    Minor changes

--- fliprLicensingApi/script.sh
+++ fliprLicensingApi/script.sh
@@ -2,6 +2,6 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input --run-syncdb
 python manage.py collectstatic --no-input
-python manage.py createsuperuser --no-input --email "rahulranjan25.RR@gmail.com" --name "Rahul R"
+python manage.py createsuperuser --no-input --email "rahulranjan25.rr@gmail.com" --name "Rahul R"
 # gunicorn fliprLicensingApi.wsgi:application --bind 0.0.0.0:8000
 python manage.py runserver 0.0.0.0:8000
